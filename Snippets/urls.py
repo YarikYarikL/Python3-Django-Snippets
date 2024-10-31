d33daaa6e7d930181be1f3f362cfa822d684cefe
Lazarev Yaroslav <yarik@mail.ru>
2024-10-31 11:07:02 +0300

    Module5:  tasks 1-10 done

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('', views.index_page, name = 'HomePage'),
+    path('snippets/add', views.add_snippet_page, name ='SnippetAdd'),
+    path('snippets/list', views.snippets_page, name ='SnippetsList'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
